a44986657b8a60f0f210c8fe028b46360fc8dd58
Arturs Olekss <arturs.olekss@gmail.com>
2021-11-01 20:53:50 +0200

    Created the first project

---
+++ StarterApp/StarterApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StarterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please, enter the text");
+            var text = Console.ReadLine();
+            foreach (char ch in text)
+            {
+                Console.WriteLine(ch);
+            }
+        }
+    }
+}
