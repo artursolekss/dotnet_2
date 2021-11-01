1ebe0e257e591562ff300966ce6af48c0207f198
Arturs Olekss <arturs.olekss@gmail.com>
2021-11-01 21:26:05 +0200

    1.st task, 1st part done

--- StarterApp/StarterApp/Program.cs
+++ StarterApp/StarterApp/Program.cs
@@ -6,12 +6,38 @@ namespace StarterApp
     {
         static void Main(string[] args)
         {
+            int leftBrackets = 0;
+            int leftBraces = 0;
+            int leftParentheses = 0;
+
+            int rightBrackets = 0;
+            int rightBraces = 0;
+            int rightParentheses = 0;
+
             Console.WriteLine("Please, enter the text");
             var text = Console.ReadLine();
             foreach (char ch in text)
             {
-                Console.WriteLine(ch);
+                if (ch == '{')
+                    leftBraces++;
+                else if (ch == '[')
+                    leftBrackets++;
+                else if (ch == '(')
+                    leftParentheses++;
+                else if (ch == '}')
+                    rightBraces++;
+                else if (ch == ']')
+                    rightBrackets++;
+                else if (ch == ')')
+                    rightParentheses++;
             }
+
+            if(leftBraces == rightBraces &&
+                leftBrackets == rightBrackets &&
+                leftParentheses == rightParentheses)
+            Console.WriteLine("Brackets are correct");
+            else
+                Console.WriteLine("Brackets are not correct");
         }
     }
 }
