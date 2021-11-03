d2525587fe603b3b27885dd748a71b10473a29fe
Arturs Olekss <arturs.olekss@gmail.com>
2021-11-03 21:23:22 +0200

    03.11.2021

---
+++ Demo1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static System.Console;
+
+namespace Demo1
+{
+    class Program
+    {
+
+        public static void WriteLine(string text)
+        {
+            ///do some stuff here
+        }
+        static void Main(string[] args)
+        {
+            Demo2.Program.WriteLine("Hello World!");
+            Console.WriteLine("Argument 1 " + args[0]);
+            Console.WriteLine("Argument 2 " + args[1]);
+
+            string text = "Text";
+            var sWitch = "3232";
+            switch (text)
+            {
+            }
+        }
+    }
+}
+
+namespace Demo2
+{
+    class Program
+    {
+
+        public static void WriteLine(string text)
+        {
+            ///do some stuff here
+        }
+        static void Methods(string[] args)
+        {
+            Demo1.Program.WriteLine("Hello World!");
+            WriteLine("Hello World!");
+        }
+    }
+}
--- StarterApp/StarterApp/Program.cs
+++ StarterApp/StarterApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StarterApp
 {
@@ -6,38 +7,52 @@ namespace StarterApp
     {
         static void Main(string[] args)
         {
-            int leftBrackets = 0;
-            int leftBraces = 0;
-            int leftParentheses = 0;
-
-            int rightBrackets = 0;
-            int rightBraces = 0;
-            int rightParentheses = 0;
-
+            LinkedList<char> brackets = new LinkedList<char>();
             Console.WriteLine("Please, enter the text");
             var text = Console.ReadLine();
             foreach (char ch in text)
             {
-                if (ch == '{')
-                    leftBraces++;
-                else if (ch == '[')
-                    leftBrackets++;
-                else if (ch == '(')
-                    leftParentheses++;
-                else if (ch == '}')
-                    rightBraces++;
-                else if (ch == ']')
-                    rightBrackets++;
-                else if (ch == ')')
-                    rightParentheses++;
+                if (IsLeftBracket(ch))
+                    brackets.AddLast(ch);
+                else if (IsRightBracket(ch))
+                {
+                    Console.WriteLine(brackets.Last.Value);
+                    if (GetRightBracket(brackets.Last.Value) != ch)
+                    {
+                        Console.WriteLine("The brackets are positioned incorrectly");
+                        return;///exit the program
+                    }
+                    else
+                        brackets.RemoveLast();
+                }
             }
-
-            if(leftBraces == rightBraces &&
-                leftBrackets == rightBrackets &&
-                leftParentheses == rightParentheses)
-            Console.WriteLine("Brackets are correct");
-            else
-                Console.WriteLine("Brackets are not correct");
+            Console.WriteLine("Brackets are positioned correctly");
         }
+
+        static bool IsLeftBracket(char ch)
+        {
+            return ch == '{' || ch == '['
+                    || ch == '(';
+        }
+
+        static bool IsRightBracket(char ch)
+        {
+            return ch == '}' || ch == ']'
+                    || ch == ')';
+        }
+
+        static char GetRightBracket(char bracket)
+        {
+            if (bracket == '{')
+                return '}';
+            else if (bracket == '[')
+                return ']';
+            else if (bracket == '(')
+                return ')';
+            else
+                return ' ';
+        }
+
+
     }
 }
