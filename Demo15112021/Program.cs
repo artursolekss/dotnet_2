fe74da61f63e86e3a584dce34c3deedb2e65cab5
Arturs Olekss <arturs.olekss@gmail.com>
2021-11-15 20:03:34 +0200

    15.11.2021

---
+++ Demo15112021/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Demo15112021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    //class MyProgram : Program,GeniusProgram <--- MULTIPLE INHERITANCE NOT ALLOWED
+    class MyProgram : Program, IComparable<MyProgram>  //MyProgram is the Sub-Class of the class Program and it implements IComparable interface (NO MULTIPLE INHERITANCE HERE)
+    {
+        public int CompareTo(MyProgram o)
+        {
+            return -1;//NO MEANING, just to escape the errors
+        }
+    }
+
+    class GeniusProgram
+    {
+
+    }
+}
